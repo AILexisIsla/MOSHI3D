64dd074280ac538b5e7427052f213ce079dabfd0
AlexisIsla <alexis.u.isla@gmail.com>
2024-11-08 13:06:23 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,42 @@
+export const slides = [
+  {
+    title: "nombre tarjeta",
+    id: "CG30128",
+  },
+  {
+    title: "nombre tarjeta",
+    id: "CG39102",
+  },
+  {
+    title: "nombre tarjeta",
+    id: "CG84729",
+  },
+  {
+    title: "nombre tarjeta",
+    id: "CG67215",
+  },
+  {
+    title: "nombre tarjeta",
+    id: "CG48391",
+  },
+  {
+    title: "nombre tarjeta",
+    id: "CG15938",
+  },
+  {
+    title: "nombre tarjeta",
+    id: "CG92457",
+  },
+  {
+    title: "nombre tarjeta",
+    id: "CG54283",
+  },
+  {
+    title: "nombre tarjeta",
+    id: "CG61872",
+  },
+  {
+    title: "nombre tarjeta",
+    id: "CG73190",
+  },
+];
